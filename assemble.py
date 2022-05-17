f104b117f2d36d7e9068c5d831d1c73b82d29f77
lopez <patrice.lopez@science-miner.com>
2022-05-17 02:59:36 +0200

    add dataseer extract

--- assemble.py
+++ assemble.py
@@ -30,6 +30,9 @@ import argparse
 import pandas as pd
 from ast import literal_eval
 from blingfire import text_to_sentences
+import xml
+from lxml import etree
+import re
 
 def process_ner_dataset_recognition(output):
     """
@@ -100,6 +103,144 @@ def process_ner_dataset_recognition(output):
 def process_dataseer(output):
     total_annotations = 0
 
+    document_paragraph = {}
+    document_paragraph["lang"] = "en"
+    document_paragraph["level"] = "paragraph"
+    document_paragraph["documents"] = []
+
+    document_sentence = {}
+    document_sentence["lang"] = "en"
+    document_sentence["level"] = "sentence"
+    document_sentence["documents"] = []
+
+    # read csv "dataset" file
+    dataset_path_csv = os.path.join("dataseer", "corpus", "csv", "stage1", "All2000_Existing-vs-New_updated_data_types_Feb5_2020.csv")
+    df = pd.read_csv(dataset_path_csv, keep_default_na=False) 
+
+    # map every dataset info belong to a document
+    document_dataset_map = {}
+
+    # Journal,ArticleNb,DOI,dataset_number,Existing/New,Full_MeSH_data_type,Section,Subsection title,Page number,Column number,Data paragraph,Data_Keyword,Data_action_word,Specialist_equipment,Notes
+    for index, row in df.iterrows(): 
+        doi = row["DOI"]
+        doi = doi.replace("https://doi.org/", "").strip()
+
+        dataset_obj = {}
+
+        dataset_label = row["Data_Keyword"].strip()
+        dataset_obj["dataset_label"] = dataset_label
+
+        if len(row["Specialist_equipment"].strip()) > 0:
+            acquisition_device_label = row["Specialist_equipment"].strip()
+            dataset_obj["acquisition_device_label"] = acquisition_device_label
+
+        dataset_type = row["Full_MeSH_data_type"].strip()
+        dataset_obj["dataset_type"] = dataset_type
+
+        '''
+        print("dataset_label:", dataset_label)
+        print("acquisition_device_label:", acquisition_device_label)
+        print("dataset_type:", dataset_type)
+        '''
+
+        if not doi in document_dataset_map:
+            document_dataset_map[doi] = []
+
+        document_dataset_map[doi].append(dataset_obj)
+
+    # match the datasets into the documents
+    for doi in document_dataset_map:
+
+        local_datasets = document_dataset_map[doi]
+
+        if len(local_datasets) == 0:
+            continue
+
+        local_document = {}
+        local_document["id"] = doi
+        local_document["body_text"] = []
+
+        # full text are in TEI XML 
+        path_tei_file = os.path.join("dataseer", "corpus", "xml", "stage1", doi.replace("/", "%2F")+".tei.xml")
+        if not os.path.exists(path_tei_file):
+            #print("cannot find file: ", doi.replace("/", "%2F")+".tei.xml")
+            continue
+
+        root = etree.parse(path_tei_file)
+
+        # get all data sentences with xpath
+        data_sentences = root.xpath('//tei:s[@id]/text()', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+        for data_sentence in data_sentences:
+
+            data_sentence = data_sentence.replace("[pagebreak]"," ")
+            data_sentence = data_sentence.replace("[page break]"," ")
+            data_sentence = data_sentence.replace("[columnbreak]"," ")
+            data_sentence = data_sentence.replace("[column break]"," ")
+            data_sentence = data_sentence.replace("\n"," ")
+            data_sentence = re.sub("( )+", " ", data_sentence)
+            data_sentence = data_sentence.strip()
+
+            annotations = []
+            spans = []
+
+            for local_dataset in local_datasets:
+                dataset_label = local_dataset["dataset_label"]
+                if "acquisition_device_label" in local_dataset:
+                    acquisition_device_label = local_dataset["acquisition_device_label"]
+
+                ind = data_sentence.find(dataset_label)
+                if ind != -1:
+                    start = ind
+                    end = ind + len(dataset_label)
+
+                    annotation = {}
+                    annotation["start"] = ind
+                    annotation["end"] = ind + len(dataset_label)
+                    annotation["text"] = data_sentence[annotation["start"]:annotation["end"]]
+                    annotation["type"] = "dataset"
+                    annotation["datatype"] = local_dataset["dataset_type"]
+
+                    # check with dataset name
+                    if annotation["text"] != dataset_label:
+                        print(str(doi), "- offset issue", ":", annotation["text"], "|", dataset_label)
+                
+                    if overlap(annotation["start"], annotation["end"], spans) == -1:
+                        annotations.append(annotation)
+                        total_annotations += 1
+                        spans.append([annotation["start"], annotation["end"]])
+
+                    if "acquisition_device_label" in local_dataset:
+                        ind2 = data_sentence.find(acquisition_device_label)
+                        if ind2 != -1:                        
+                            start2 = ind2
+                            end2 = ind2 + len(acquisition_device_label)
+
+                            # do something
+                            annotation2 = {}
+                            annotation2["start"] = ind2
+                            annotation2["end"] = ind2 + len(acquisition_device_label)
+                            annotation2["text"] = data_sentence[annotation2["start"]:annotation2["end"]]
+                            annotation2["type"] = "data_acquisition_device"
+                            annotation2["datatype"] = local_dataset["dataset_type"]
+
+                            annotations.append(annotation2)
+                            #total_annotations += 1
+            
+            if len(annotations)>0:
+                new_section_part = {}
+                #new_section_part["section_title"] = section_title
+                new_section_part["text"] = data_sentence
+                new_section_part["annotation_spans"] = annotations
+
+                local_document["body_text"].append(new_section_part)
+
+        if len(local_document["body_text"])>0:
+            document_sentence["documents"].append(local_document)
+
+    output_path = os.path.join(output, "dataseer_sentences.json")
+    with open(output_path,'w') as out:
+        out.write(json.dumps(document_sentence, indent=4))
+
     print("\ntotal annotations dataseer:", str(total_annotations))
 
 
@@ -141,6 +282,7 @@ def process_coleridge(output):
     df = pd.read_csv(dataset_path_csv) 
 
     # Id,pub_title,dataset_title,dataset_label,cleaned_label
+
     # Id is actually an identifier of the publication and it is repeated in several raw, one by dataset "form" in this document
     # it seems that we have to match the dataset surface form to the text content
     # no offset, no inline annotation, etc. a sort of archaic/naive format
@@ -164,6 +306,7 @@ def process_coleridge(output):
     # match the datasets into the documents
     for doc_id in document_dataset_map:
 
+        # this never happens
         if not doc_id in document_dataset_map:
             continue
 
@@ -200,7 +343,6 @@ def process_coleridge(output):
                     # used to check if the dataset name is not a sub-string of an already existing
                     # dataset name, or on the contrary extends an existing one
                     spans = []
-
                     annotations = []
 
                     for local_dataset in local_datasets:
